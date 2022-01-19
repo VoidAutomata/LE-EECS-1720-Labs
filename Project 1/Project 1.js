d7485196270df20b8281623b56f316e6f6fae946
VoidAutomata <82106210+VoidAutomata@users.noreply.github.com>
2022-01-19 17:06:52 -0500

    17:04 2022-01-19

--- Project 1/Project 1.js
+++ Project 1/Project 1.js
@@ -8,6 +8,12 @@ if (urlParams.has('q')){
     let xhr = new XMLHttpRequest();
 
     xhr.onload = function(){
-        let results = Array.from(this.responseXML.getElementsByClassName('result'))
+        let results = Array.from(this.responseXML.getElementsByClassName('result')).map(result => result.getElementsByClassName('result__a')[0].href);
+        results = results.filter(result => !result.startsWith('https://duckduckgo.com/y.js'));
+        applyResults(results);
+    }
+
+    xhr.onerror = function (){
+        console.log("an error occured");
     }
 }
