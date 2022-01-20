e1b62814501b4080b3ee325da077da8368783b26
VoidAutomata <82106210+VoidAutomata@users.noreply.github.com>
2022-01-20 16:51:29 -0500

    span inclusion, alert
    -Includes span elements, highlighting and searching through them
    
    -Will alert of all numbers found (currently repeats for every character in the element, will fix later)

--- Project 1/Project 1.js
+++ Project 1/Project 1.js
@@ -1,10 +1,31 @@
 console.log("active");
+var numbers = "";
 
 let paragraph = document.getElementsByTagName('p');
-for (elt of paragraph){
-    let pattern = elt.textContent.match(/\d+/g);
-    if(pattern != null){
-        elt.style['color'] = '#00ff00';
+let span = document.getElementsByTagName('span');
+for (elt of paragraph) {
+    patternExtract(elt);
+}
+for (elt of span){
+    patternExtract(elt);
+}
+
+alert(numbers);
+
+function patternExtract (elt){
+    elt.style['color'] = '#ff0000';
+
+
+    for (i of elt.textContent) {
+        let pattern = elt.textContent.match(/\d+/g);
+        if (pattern != null) {
+            elt.style['color'] = '#00ff00';
+            for (char of pattern){
+                let pattern2 = char.match(/\d+/g);
+                if (pattern2 != null){
+                    numbers += pattern2 + ", ";
+                }
+            }
+        }
     }
-    else {elt.style['color'] = '#ff0000';}
 }
