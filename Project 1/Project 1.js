bb865819a63feab8b6c3fbccf487fa61059bc38c
VoidAutomata <82106210+VoidAutomata@users.noreply.github.com>
2022-01-20 12:12:27 -0500

    Red Paragraphs
    Changes all tag elements p to have red background.

--- Project 1/Project 1.js
+++ Project 1/Project 1.js
@@ -1,19 +1,6 @@
-const qString = window.location.search;
+console.log("active");
 
-const urlParams = new URLSearchParams(qString);
-
-if (urlParams.has('q')){
-    const searchQuery = urlParams.get('q');
-
-    let xhr = new XMLHttpRequest();
-
-    xhr.onload = function(){
-        let results = Array.from(this.responseXML.getElementsByClassName('result')).map(result => result.getElementsByClassName('result__a')[0].href);
-        results = results.filter(result => !result.startsWith('https://duckduckgo.com/y.js'));
-        applyResults(results);
-    }
-
-    xhr.onerror = function (){
-        console.log("an error occured");
-    }
+let paragraph = document.getElementsByTagName('p');
+for (elt of paragraph){
+    elt.style['background-color'] = '#ff0000';
 }
