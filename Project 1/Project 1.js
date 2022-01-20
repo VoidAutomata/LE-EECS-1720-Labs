431b3122842bec782524f4ab5c952fca35fceb8d
VoidAutomata <82106210+VoidAutomata@users.noreply.github.com>
2022-01-20 16:11:40 -0500

    green number paragraphs
    Paragraphs that contain numbers now have #00ff00 text colour.

--- Project 1/Project 1.js
+++ Project 1/Project 1.js
@@ -2,5 +2,9 @@ console.log("active");
 
 let paragraph = document.getElementsByTagName('p');
 for (elt of paragraph){
-    elt.style['color'] = '#ff0000';
+    let pattern = elt.textContent.match(/\d+/g);
+    if(pattern != null){
+        elt.style['color'] = '#00ff00';
+    }
+    else {elt.style['color'] = '#ff0000';}
 }
