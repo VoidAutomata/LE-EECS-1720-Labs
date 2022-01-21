f5a5883c23224d4aa46b292c3225d5ac8d06371c
VoidAutomata <82106210+VoidAutomata@users.noreply.github.com>
2022-01-21 12:55:05 -0500

    no repeat
    Fixed an issue with numbers repeating for each char in a element

--- Project 1/Project 1.js
+++ Project 1/Project 1.js
@@ -1,30 +1,32 @@
 console.log("active");
 var numbers = "";
+let regex = /\d+/g;
 
 let paragraph = document.getElementsByTagName('p');
 let span = document.getElementsByTagName('span');
 for (elt of paragraph) {
     patternExtract(elt);
 }
-for (elt of span){
+for (elt of span) {
     patternExtract(elt);
 }
 
 alert(numbers);
 
-function patternExtract (elt){
+function patternExtract(elt) {
     elt.style['color'] = '#ff0000';
 
 
-    for (i of elt.textContent) {
-        let pattern = elt.textContent.match(/\d+/g);
-        if (pattern != null) {
-            elt.style['color'] = '#00ff00';
-            for (char of pattern){
-                let pattern2 = char.match(/\d+/g);
-                if (pattern2 != null){
-                    numbers += pattern2 + ", ";
-                }
+
+    let pattern = elt.textContent.match(regex);
+    if (pattern != null) {
+        elt.style['color'] = '#00ff00';
+        for (char of pattern) {
+            let pattern2 = char.match(regex);
+            if (pattern2 != null) {
+                numbers += pattern2 + ", ";
+                pattern2 = null;
+                //break;
             }
         }
     }
